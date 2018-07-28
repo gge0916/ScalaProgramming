112a707353bd8e7bdaa2c1aa99f255b0642a6bc4
Gongda Ge <gegongda92@gmail.com>
2018-07-28 15:52:04 -0400

    finish

--- Practices/recursion/src/main/scala/recfun/Main.scala
+++ Practices/recursion/src/main/scala/recfun/Main.scala
@@ -13,15 +13,55 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(c: Int, r: Int): Int = {
+      if (c > r) 0 // Out of the triangle
+      else if (c == 0 || c == r) 1
+      else pascal(c - 1, r - 1) + pascal(c, r - 1)
+    }
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+      def helper(chars: List[Char], openCount: Int): Boolean = {
+        if (chars.isEmpty) {
+          openCount == 0
+        } else {
+          val head = chars.head
+          val newOpenCount =
+            if (head == '(') openCount + 1
+            else if (head == ')') openCount - 1
+            else openCount
+          // ')' should never be more than '('
+          if (newOpenCount < 0) false
+          else helper(chars.tail, newOpenCount)
+        }
+      }
+      helper(chars, 0)
+    }
   
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int = {
+      def helper(money: Int, sortedCoins: List[Int]): Int = {
+        if (money == 0) {
+          1
+        }
+        else if (sortedCoins.isEmpty) {
+          if (money == 0) 1
+          else 0
+        } else {
+          val first = sortedCoins.head
+          if (money < first) {
+            0
+          } else {
+            helper(money - first, sortedCoins) + helper(money, sortedCoins.tail)
+          }
+        }
+
+      }
+      val sortedCoins = coins.sorted
+      helper(money, sortedCoins)
+    }
   }
