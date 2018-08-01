696e86771a7e49884e72b357b6ab475a4e53a0dd
Gongda Ge <gegongda92@gmail.com>
2018-07-31 21:59:28 -0400

    finish hw for week2

--- Practices/functional_sets/src/main/scala/funsets/FunSets.scala
+++ Practices/functional_sets/src/main/scala/funsets/FunSets.scala
@@ -19,31 +19,31 @@ object FunSets {
   /**
    * Returns the set of the one given element.
    */
-    def singletonSet(elem: Int): Set = ???
+    def singletonSet(elem: Int): Set = (num: Int) => num == elem
   
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-    def union(s: Set, t: Set): Set = ???
+    def union(s: Set, t: Set): Set = (num: Int) => contains(s, num) || contains(t, num)
   
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-    def intersect(s: Set, t: Set): Set = ???
+    def intersect(s: Set, t: Set): Set = (num: Int) => contains(s, num) && contains(t, num)
   
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-    def diff(s: Set, t: Set): Set = ???
+    def diff(s: Set, t: Set): Set = (num: Int) => contains(s, num) && !contains(t, num)
   
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-    def filter(s: Set, p: Int => Boolean): Set = ???
+    def filter(s: Set, p: Int => Boolean): Set = (num: Int) => contains(s, num) && p(num)
   
 
   /**
@@ -56,23 +56,23 @@ object FunSets {
    */
     def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if (a > bound) true
+      else if (contains(s, a)) p(a) && iter(a + 1)
+      else iter(a + 1)
     }
-    iter(???)
+    iter(-1000)
   }
   
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-    def exists(s: Set, p: Int => Boolean): Boolean = ???
+    def exists(s: Set, p: Int => Boolean): Boolean = !forall(s, (num: Int) => !p(num))
   
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-    def map(s: Set, f: Int => Int): Set = ???
+    def map(s: Set, f: Int => Int): Set = (num: Int) => exists(s, (num1: Int) => f(num1) == num)
   
   /**
    * Displays the contents of a set
